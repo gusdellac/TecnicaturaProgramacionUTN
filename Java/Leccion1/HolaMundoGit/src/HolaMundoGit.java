689a9fa67f2b55c1a601262503a5f0ba359566fd
Gustavo De Llac <gustavodellac@gmail.com>
2022-05-05 16:44:51 -0300

    Modificiación archivo java clase 4

--- Java/Leccion1/HolaMundoGit/src/HolaMundoGit.java
+++ Java/Leccion1/HolaMundoGit/src/HolaMundoGit.java
@@ -74,6 +74,7 @@ public class HolaMundoGit {
         */
         
         //Clase Scanner
+        /*
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite su nombre: ");
         
@@ -82,7 +83,7 @@ public class HolaMundoGit {
         Esta clase es necesario importarla. Se encuentra en otro
         paquete llamado java.util.scanner
         utilizamos este objeto para poder leer datos ingresados desde la 
-        consola*/
+        consola
         
         var usuario2 = entrada.nextLine(); //Llamamos al objeto más el método
         //nextLine()
@@ -102,11 +103,50 @@ public class HolaMundoGit {
         var autor = input.nextLine();
         
         System.out.println(libro+" fué escrito por "+autor);
+        */
+        
+        /*
+        byte numEnteroByte = 127;//definimos un tipo de dato byte
+        //colocando byte + variable definida. El (byte) obliga al compilador a que 
+        //convierta el dato asignado a tipo byte. Si el valor asignado supera el máximo
+        //o mínimo permitido se generará pérdida de precisión
+        System.out.println("numEnteroByte = " + numEnteroByte);
+        System.out.println("valor mínimo del Byte:" + Byte.MIN_VALUE);//-128
+        System.out.println("valor máximo del Byte:" + Byte.MAX_VALUE);//127
+        //Byte.MIN_VALUE para poder evaluar los mínimos y maximos del tipo de dato
+        //es necesario que utilicemos la clase Byte
+        
+        short numEnteroShort = 32767;
+        System.out.println("numEnteroShort = " + numEnteroShort);
+        System.out.println("Valor mínimo del Short:" + Short.MIN_VALUE); //-32768
+        System.out.println("Valor máximo del Short:" + Short.MAX_VALUE); //32767
+        
+        int numEnteroInt = 2147483647;
+        System.out.println("numEnteroInt = " + numEnteroInt);
+        System.out.println("Valor mínimo del int:" + Integer.MIN_VALUE); //-2147483648
+        System.out.println("Valor máximo del int:" + Integer.MAX_VALUE); //2147483647
+        //java por defecto trabajo con los números enteros int de 32 bits
+        
+        long numEnteroLong = 9223372036854775807L; //Java por defecto asigna el máximo de números enteros
+        //a los int, por lo que al asignar un número tipo long es necesario colocar
+        //la L al final del número.
+        System.out.println("numEnteroLong = " + numEnteroLong);
+        System.out.println("Valor mínimo del long:" + Long.MIN_VALUE); //-9223372036854775808
+        System.out.println("Valor mínimo del long:" + Long.MAX_VALUE); //9223372036854775807
+        */
+        
+        float numFloat = (float) 3.4028235E38F; //Por defoult java asigna los datos decimales al tipo double
+        //se forza a float usando (float) o F al final del número.
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("El valor mínimo de float:" + Float.MIN_VALUE); //1.4E-45
+        System.out.println("El valor máximo de float:" + Float.MAX_VALUE); //3.4028235E38
         
         
+        double numDouble = 1.7976931348623157E308D;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("El valor mínimo del double:" + Double.MIN_VALUE); //4.9E-324
+        System.out.println("El valor máxmio del double:" + Double.MAX_VALUE); //1.7976931348623157E308
         
-        
-
     }
 
 }
