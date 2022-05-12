032cbca1244ca3fea04dfc4ce136ae0748ca048c
Gustavo De Llac <gustavodellac@gmail.com>
2022-05-12 17:04:29 -0300

    agregue los id al ejercicio3

--- python/Leccion2/Ejercicio3.py
+++ python/Leccion2/Ejercicio3.py
@@ -7,9 +7,13 @@ print(f"El valor de b es: {b}")
 aux = a
 a = b
 b = aux
+print(id(a))
+print(id(b))
 print(f"El nuevo valor de a es: {a}")
 print(f"El nuevo valor de b es: {b}")
 # Intercambiamos los valores de las variables en la misma línea sin tener que utilizar un auxiliar
 a, b = b, a  # Notar que la reasignación se realiza al mismo tiempo
+print(id(a))
+print(id(b))
 print(f"El nuevo valor de a es: {a}")
 print(f"El nuevo valor de b es: {b}")
