e4f682f3445340e37faef9734fd974770ef015bd
Gustavo De Llac <gustavodellac@gmail.com>
2022-08-24 16:48:51 -0300

    commit archivos python clase 2 Semestre 2

--- python/Leccion4/main.py
+++ python/Leccion4/main.py
@@ -1,3 +1,5 @@
+# COLECCIONES DE DATOS EN PYTHON
+
 # Listas (arrays) . Las listas son mutables
 nombres = ['Marcela', 'Roman', 'Fernanda', 'Leonardo']
 print('Listas o arrays')
@@ -5,9 +7,9 @@ print(nombres[0])  # Mostramos el primer elemento
 print(nombres[2])  # Mostramos el tercer elemento
 print(nombres[-1])  # Mostramos el último elemento
 print(nombres[-2])  # Mostramos el penúltimo elemento
-print(nombres[ :2])  # Mostramos el elemento del índice 0 y 1
-print(nombres[ :3])  # Mostramos el elemento del índice 0 y 1
-print(nombres[1: ])  # Mostramos el elemento del índice 1 en adelante
+print(nombres[:2])  # Mostramos el elemento del índice 0 y 1
+print(nombres[:3])  # Mostramos el elemento del índice 0 y 1
+print(nombres[1:])  # Mostramos el elemento del índice 1 en adelante
 nombres[2] = 'Ricardo'  # Modificamos un valor
 print(nombres)
 print('\n')
@@ -20,9 +22,16 @@ else:
 # Longitud array
 print(f'La lista o array tiene: {len(nombres)} elementos')  # Método len()
 
-# Agregar elemento a array
+# Agregar elemento. Se pueden agregar diferentes tipos de datos
 nombres.append('Marcelo')  # Método append()
+nombres.append([1, 2, 3])  # Agregamos una lista en una posición de la lista anterior
+nombres.append(True)  # dato booleano
+nombres.append(10.45)  # dato numérico flotante
+nombres.append([4, 5])  # lista
+nombres.append(7)  # entero
 print(nombres)
+print(nombres[5])  # print de la lista agregada en la posición 5 del array
+print(nombres[5][2])  # print de la posición 2 de la lista guardada en la posición 5 del array
 
 # Insertar un elemento en un índice específico
 nombres.insert(1, 'Alberto')  # Método insert()
@@ -49,6 +58,38 @@ print(nombres)
 # Eliminar lista
 del nombres
 
+# Concatenamos listas
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+lista3 = lista1 + lista2
+print(lista3)
+
+# Método extend(). Agregamos elementos a una lista
+lista3.extend([7, 8, 9])
+print(lista3)
+
+print(lista3.index(5))  # método para ubicar en que índice esta el valor ingresado
+# print(lista3.index(.))  # esto genera error porque el elemento no esta en la lista
+
+# Como saber si un valor indicado esta repetido y cuantas veces lo está.
+print(lista3.count(1))  # Método count()
+
+# Para invertir una lista. Método reverse()
+lista3.reverse()
+print(lista3)
+
+# Para que una lista se multiplique repitiendo sus elementos
+lista = [1, 2, 3] * 2
+print(lista)
+
+# Métodos de ordenamiento
+lista.sort()  # método sort() .Ordena los elementos ascendentemente
+print(lista)
+lista.sort(reverse=True)  # método sort() con parámetro reverse=True . Ordena los elementos descendentemente
+print(lista)
+
+
+
 # Tupla . Son listas no mutables
 print('\n')
 print('Tuplas:')
@@ -74,16 +115,107 @@ for cocinar in cocina:
     # realizan los saltos de línea del print. Ejemplo end=' ' , se verá en una sola línea con un espacio
 
 # Modificar elementos de una tupla (es una mala práctica)
-cocinaLista = list(cocina)
+cocinaLista = list(cocina)  # Convertimos la tupla a lista
 print(type(cocinaLista))
-cocinaLista[0] = 'Plato'
-cocina = tuple(cocinaLista)
+cocinaLista[0] = 'Plato'  # Modificamos un valor de la lista
+cocina = tuple(cocinaLista)  # Convertimos la lista a tupla
 print(type(cocina))
+
+# Tupla con elementos de distinto tipo de dato
+tupla = (4, 'Hola', 6.78, [1, 2, 78], 4, 'Hola')
+
+print(4 in tupla)  # Buscamos un elemento. Devuelve un dato booleano
 
 # Eliminar tupla
 del cocina
 
+# Se puede usar con tuplas los métodos index() count() len()
 
+# Tipo set : Se pueden almacenar datos de forma similar a las listas y tuplas.
+# No se almacenan datos repetidos y los datos almacenados no tienen un orden (no tienen índice)
+planetas = {'Marte', 'Júpiter', 'Venus'}
+print(planetas)
+# método len()
+longitudPlanetas = len(planetas)
+print(longitudPlanetas)
 
+# Verificar si un elemento existe dentro del conjunto set
+print('Marte' in planetas)
+print('Marte' not in planetas)  # Forma negativa
 
+# Agregar un elemento, método add()
+planetas.add('Tierra')
+print(planetas)
+
+# Eliminar elementos
+# Método remove() , arroja error si el elemento no existe
+planetas.remove('Júpiter')
+print(planetas)
+# Método discard() , no arroja error si el elemento no existe
+planetas.discard('Tierra')
+print(planetas)
+
+# Limpiar set o conjunto
+planetas.clear()
+print(planetas)
+
+# Eliminar set o conjunto
+del planetas
+
+# Diccionarios. Está cumpuesto por dos elementos -(llave : valor)
+# dict(key,value)
+# Los diccionarios no tienen índice, se accede a los elementos mediante las llaves
+diccionario = {
+    'IDE': 'Integrated Development Environment',
+    'POO': 'Programación Orientada a Objetos',
+    'SABD': 'Sistema de Administración de Base de Datos'
+}
+# Verificar la cantidad de elementos de un diccionario
+print(len(diccionario))
+print(diccionario)
+# Acceder a un diccionario con la llave(key)
+print(diccionario['IDE'])
+
+# Otra forma de recuperar un elemento. Método get()
+print(diccionario.get('POO'))
+print(diccionario.get('SABD'))
+
+# Modificamos elementos
+diccionario['IDE'] = 'Entorno de Desarrollo Integrado'
+print(diccionario)
+
+# Como recorrer los elementos
+for termino in diccionario:  # Recorremos el diccionario mostrando solo las llaves(key)
+    print(termino)
+# Necesitamos un método para recorrer el diccionario y poder acceder al valor de las llaves. Método items()
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+# Otras maneras de acceder a un diccionario.
+
+# Método keys(). Accedemos a key
+for termino in diccionario.keys():
+    print(termino)  # Muestra solo las llaves
+
+# Método value() . Accedemos a value
+for valor in diccionario.values():
+    print(valor)
+
+# Comprobar la existencia de algún elemento
+print('IDE' in diccionario)  # devuelve dato booleano
+
+# Agregar un elemento
+diccionario['PK'] = 'Primary Key'
+print(diccionario)
+
+# Eliminar un elemento. Método pop()
+diccionario.pop('SABD')
+print(diccionario)
+
+# Vaciar un diccionario. Método clear()
+diccionario.clear()
+print(diccionario)
+
+# Eliminar diccionario
+del diccionario
 
