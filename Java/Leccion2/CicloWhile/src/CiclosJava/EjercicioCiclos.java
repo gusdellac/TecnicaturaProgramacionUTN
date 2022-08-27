93f98a8b955fb0696d2720419b31740d17002c94
Gustavo De Llac <gustavodellac@gmail.com>
2022-08-27 14:01:19 -0300

    commit archivos Java clase 1-b semestre 2

--- Java/Leccion2/CicloWhile/src/CiclosJava/EjercicioCiclos.java
+++ Java/Leccion2/CicloWhile/src/CiclosJava/EjercicioCiclos.java
@@ -25,6 +25,43 @@ public class EjercicioCiclos {
         for(var iterador = 0;iterador < 7; iterador++){
             System.out.println("iterador = " + iterador);
         }
+        
+        //palabra reservada break
+        System.out.println("Break: ");
+        for(var iterador = 0;iterador < 7; iterador++){
+            if (iterador % 2 == 0){ //condicional con operador de igualdad
+                System.out.println("iterador = " + iterador);
+                break;
+            }  
+        }
+        //palabra reservada continue
+        System.out.println("Continue: ");
+        for(var iterador = 0;iterador < 7; iterador++){
+           if (iterador % 2 != 0){ // condicional con operador de negación
+               continue; //continúa con la iteración
+           }
+           System.out.println("iterador = " + iterador);
+        }
+        
+        //Etiquetas (labels). Tienen utilidad con ciclos anidados
+        
+        System.out.println("Labels con break: ");
+        inicio:
+        for(var iterador = 0;iterador < 7; iterador++){
+            if (iterador % 2 == 0){ //condicional con operador de igualdad
+                System.out.println("iterador = " + iterador);
+                break inicio;
+            }  
+        }
+        
+        System.out.println("Labels con continue: ");
+        inicio:
+        for(var iterador = 0;iterador < 7; iterador++){
+           if (iterador % 2 != 0){ 
+               continue inicio;
+           }
+           System.out.println("iterador = " + iterador);
+        }
     }
     
 }
